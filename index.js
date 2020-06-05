1aa22ef226b79dff1c663295c841b6f7d06e107d
Rishabh Jain <rishabh@jnexsoft.com>
2020-06-05 22:07:16 +0530

    refactor(app): updated routes to use new codebase

--- index.js
+++ index.js
@@ -1,140 +1,28 @@
-const { google } = require('googleapis');
 const express = require('express');
 const app = express();
+const groupBy = require('lodash/groupBy');
+const camelCase = require('lodash/camelCase');
+const mapKeys = require('lodash/mapKeys');
+const middleware = require('./middleware');
+const { getSpeakers, getEvents } = require('./services/googleSheet');
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log('Listening!', port);
-});
 
-app.get('/', async (req, res) => {
-    res.send('DevTalks-Events-API');
-});
-
-/*
-app.get('/hello', async (req, res) => {
-    const name = req.query.name;
-    const result = `Hello ${name}\n\n`;
-    res.send(result);
-});
-*/
+app.use(middleware);
 
 app.get('/events', async (req, res) => {
-    const events = await getRow();
-    let retval;
-    if (events) {
-        retval = {
-            status: 'success',
-            data: { events: events }
-        }
-    } else {
-        retval = {
-            status: 'error',
-            data: { events: 'nothing' }
-        }
-    }
-    res.setHeader('content-type', 'application/json');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET');
-    res.send(JSON.stringify(retval));
+  const events = await getEvents();
+  let groupedEvents = groupBy(events, 'pastUpcoming');
+  groupedEvents = mapKeys(groupedEvents, (val, key) => camelCase(key));
+
+  res.api({ events: groupedEvents });
 });
-
-async function getRow() {
-    const auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-
-    const api = google.sheets({ version: 'v4', auth });
-    const response = await api.spreadsheets.values.get({
-        spreadsheetId: '1e2GXQAvCEeJ-iUtQzTCSI_US-6Hh1K_22rYbbokyzj0',
-        range: 'Events!A:G'
-    });
-
-    let frow = true;
-    let past = [];
-    let upcoming = [];
-    for (let row of response.data.values) {
-	if(frow) {
-	    frow = false;
-	    continue;
-	}
-	if(row[5] == 'Disabled') {
-	    continue;
-	}
-	if(row[3] == 'Past') {
-            past.push({
-                title: row[0],
-                date: row[1],
-                speaker: row[2],
-                link: row[4],
-                id: row[6],
-            });
-	} else {
-	    upcoming.push({
-		title: row[0],
-		date: row[1],
-		speaker: row[2],
-		link: row[4],
-                id: row[6],
-	    });
-	}
-    }
-    return {past: past, upcoming: upcoming};
-}
-
 
 app.get('/speakers', async (req, res) => {
-    const speakers = await getSpeakers();
-    let retval;
-    if (speakers) {
-        retval = {
-            status: 'success',
-            data: { speakers: speakers }
-        }
-    } else {
-        retval = {
-            status: 'error',
-            data: { speakers: 'nothing' }
-        }
-    }
-    res.setHeader('content-type', 'application/json');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET');
-    res.send(JSON.stringify(retval));
+  const speakers = await getSpeakers();
+  res.api({ speakers });
 });
 
-async function getSpeakers() {
-    const auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-
-    const api = google.sheets({ version: 'v4', auth });
-    const response = await api.spreadsheets.values.get({
-        spreadsheetId: '1e2GXQAvCEeJ-iUtQzTCSI_US-6Hh1K_22rYbbokyzj0',
-        range: 'Speakers!A:F'
-    });
-
-    let frow = true;
-    let speakers = [];
-    for (let row of response.data.values) {
-        if(frow) {
-            frow = false;
-            continue;
-        }
-        if(row[6] == 'Disabled') {
-            continue;
-        }
-        speakers.push({
-            name: row[0],
-            twitter: row[1],
-            linkedin: row[2],
-            github: row[3],
-            personal: row[4],
-            photo: row[5]
-        });
-    }
-    return speakers;
-}
-
+app.listen(port, () => {
+  console.log('Listening!', port);
+});
