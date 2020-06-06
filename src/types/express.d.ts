ca310b76f198d088afe993a15c7b0097548d61ae
Rishabh Jain <rishabh@jnexsoft.com>
2020-06-07 03:42:51 +0530

    feat(types): extended express Response interface types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -5,8 +5,8 @@ export {};
 declare global {
   namespace Express {
     interface Response {
-      api(req: Request, res: Response, next: NextFunction): void;
-      error(req: Request, res: Response, next: NextFunction): void;
+      api(body: Record<string, any> | Array<Record<string, any>>): void;
+      error(body: Record<string, any> | Array<Record<string, any>>): void;
     }
   }
 }
