a2b5dad7099a66646485257e1b49f62e87800e30
Rishabh Jain <rishabh@codeation.io>
2020-08-06 03:38:24 +0530

    feat: added body parser

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,12 @@
+import bodyParser from 'body-parser';
 import { compose } from 'compose-middleware';
 import cors from 'cors';
 import responseMiddleWare from './responseMiddleware';
 
 const middleware = [
   cors() as any,
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: true }),
   responseMiddleWare
 ];
 
